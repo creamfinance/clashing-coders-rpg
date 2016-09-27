815bb07e9d70c8ee061e086e99c1e83041017e43
Markus Lechner <markus.lechner@creamfinance.com>
2016-09-27 14:56:41 +0200

    added level 4 and 6 and interaction

---
+++ levels/6.js
@@ -0,0 +1,29 @@
+module.exports = {
+    init: function () {
+        this.players[0].inventory.keys = 0;
+    },
+    player_definition: [{ x:1, y:99 }],
+    isFinished: function (players) {
+        //var fin = false;
+        return players[0].position.x == 95 && players[0].position.y == 5 && players[0].steps <= 900; //steps <= 900
+    },
+    processAction: function (player, action, options) {
+        options = options || {};
+
+        switch (action.toLowerCase()) {
+            case 'up':
+            case 'down':
+            case 'left':
+            case 'right':
+                this.movePlayer(player, action, options);
+                return true;
+            case 'interact':
+                this.interactPlayer(player);
+                return true;
+            default:
+                return false;
+        }
+    },
+    movePlayer: require('./movePlayer'),
+    interactPlayer: require('./interactPlayer'),
+};
