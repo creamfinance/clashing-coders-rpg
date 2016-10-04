cc8c7c4620593908f0702115b1d3d58c8e1eca9c
RianFuro <florian.proksch@outlook.com>
2016-10-04 12:25:19 +0200

    license added to files

--- controllers/AuthenticationController.js
+++ controllers/AuthenticationController.js
@@ -1,9 +1,27 @@
+/*
+    Clashing Coders RPG Platform - The platform used for Creamfinance's first coding contest.
+    Copyright (C) 2016 Florian Proksch
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 var Class = require('class'),
     bcrypt = require('bcrypt'),
     crypto = require('crypto'),
     pool = require('../pool'),
     redis = require('../redis'),
-    users = require('../users');
+    users = require('../repositories/UserRepository');
 
 var AuthenticationController = Class.bind(null, 'AuthenticationController', Object);
 
